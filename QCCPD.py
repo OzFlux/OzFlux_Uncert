919fefb76f8dc79b30b1c6748a1012bb4e81169c
imchugh <ian.mchugh@monash.edu>
2014-08-08 13:37:44 +1000

    Working file import, application of QC and return of df to main routine

--- QCCPD.py
+++ QCCPD.py
@@ -1,11 +1,119 @@
-# My modules
-import ustar_plots
+# Python modules
+import Tkinter, tkFileDialog
+from configobj import ConfigObj
+import netCDF4
+import xlrd
+import datetime as dt
+import ast
 import numpy as np
 import pandas as pd
 from scipy import stats
 import pdb
 import os
 
+# Fetch the data and prepare it for analysis
+def CPD_run():
+        
+    # Prompt user for configuration file and get it
+    root = Tkinter.Tk(); root.withdraw()
+    cfName = tkFileDialog.askopenfilename(initialdir='')
+    root.destroy()
+    cf=ConfigObj(cfName)
+    
+    # Set input file and output path and create directories for plots and results
+    file_in=os.path.join(cf['files']['input_path'],cf['files']['input_file'])
+    path_out=cf['files']['output_path']
+    plot_path_out=os.path.join(path_out,'Plots')
+    if not os.path.isdir(plot_path_out): os.makedirs(os.path.join(path_out,'Plots'))
+    results_path_out=os.path.join(path_out,'Results')
+    if not os.path.isdir(results_path_out): os.makedirs(os.path.join(path_out,'Results'))    
+    
+    # Get user-set variable names from config file
+    vars_data=[cf['variables']['data'][i] for i in cf['variables']['data']]
+    vars_QC=[cf['variables']['QC'][i] for i in cf['variables']['QC']]
+    vars_all=vars_data+vars_QC
+       
+    # Read .nc file
+    nc_obj=netCDF4.Dataset(file_in)
+    flux_frequency=int(nc_obj.time_step)
+    dates_list=[dt.datetime(*xlrd.xldate_as_tuple(elem,0)) for elem in nc_obj.variables['xlDateTime']]
+    d={}
+    for i in vars_all:
+        d[i]=nc_obj.variables[i][:]
+    nc_obj.close()
+    df=pd.DataFrame(d,index=dates_list)    
+        
+    # Build dictionary of additional configs
+    d={}
+    d['radiation_threshold']=int(cf['options']['radiation_threshold'])
+    d['num_bootstraps']=int(cf['options']['num_bootstraps'])
+    d['flux_frequency']=flux_frequency
+        
+    # Replace configured error values with NaNs and remove data with unacceptable QC codes, then drop flags
+    df.replace(int(cf['options']['nan_value']),np.nan)
+    if 'QC_accept_codes' in cf['options']:    
+        QC_accept_codes=ast.literal_eval(cf['options']['QC_accept_codes'])
+        eval_string='|'.join(['(df[vars_QC[i]]=='+str(i)+')' for i in QC_accept_codes])
+        for i in xrange(4):
+            df[vars_data[i]]=np.where(eval(eval_string),df[vars_data[i]],np.nan)
+    df=df[vars_data]
+    
+    return df,d
+
+    # Return a bootstrapped sample of the passed dataframe
+def bootstrap(sample_df):
+    return sample_df.iloc[np.random.random_integers(0,len(sample_df)-1,len(sample_df))]
+
+def CPD_main():
+
+    df,d=CPD_run()
+    
+    ### Bootstrap the data and run the CPD algorithm
+    for i in xrange(bootstrap_n):
+        
+        # Use the observed data as the first sample
+        bootstrap_flag=(False if i==0 else True)
+        
+        # Bootstrap the data for each year
+        if bootstrap_flag==False:
+            print 'Using observational data for first pass'
+        else:
+            df=pd.concat([bootstrap(df.ix[str(j)]) for j in years_index])
+            print 'Generating bootstrap '+str(i)
+        
+        # Create nocturnal dataframe (drop all records where any one of the variables is NaN)
+        df=df[['Fc','Ta','ustar']][df['Fsd']<rad_threshold].dropna(how='any',axis=0)        
+        
+#                
+#        
+#        # Organise data into years, seasons and temperature strata, sort by temperature and u* and average across bins
+#        print 'Sorting data'
+#        try:
+#            years_df,seasons_df,results_df=analysis_prep.sort_data(noct_df,fluxfreq)       
+#        except StandardError:
+#            # data noct_df is problematic
+#            continue
+#        
+#        # Use the results df index as an iterator to run the CPD algorithm on the year/season/temperature strata
+#        print 'Finding change points'
+#        fitData_array=np.vstack([CPD.fits(seasons_df.ix[i[0]].ix[i[1]].ix[i[2]],i[0],i[1],i[2],bootstrap_flag,plot_out) for i in results_df.index])
+#        cols=['bMod_threshold','bMod_f_max','b1','bMod_CP','aMod_threshold','aMod_f_max','norm_a1','norm_a2','a1p','a2p']
+#        
+#        # Recompose the results dataframe with fit results
+#        results_df=results_df.reset_index(level=['season','T_class'],drop=True) 
+#        temp_S=results_df['T_avg']
+#        temp_index=results_df.index
+#        results_df=pd.DataFrame(fitData_array,columns=cols,index=temp_index)
+#        results_df['T_avg']=temp_S
+#
+#        # QC the results and strip remaining extraneous columns
+#        print 'Doing QC within bootstrap'
+#        results_df=CPD_QC.QC1(results_df)
+#        
+#        # Run the CPD algorithm and return results
+#        interm_list.append(results_df)
+#        counts_list.append(years_df['seasons']*4)
+    
 # Find change point for model with slope above change point (Barr's
 # 'diagnostic' model)
 
@@ -13,9 +121,6 @@ def fits(temp_df,year,season,T_class,bootstrap_flag,plot_out_path):
     
     # Only works if the index is reset here (bug?)!
     temp_df=temp_df.reset_index(drop=True)
-   
-    #if (year==2011)&(season==3)&(T_class==3):
-    #    temp_df.to_pickle(os.path.join('C:\Temp\Results','temp.df'))        
        
     ### Calculate null model SSE for operational (b) and diagnostic (a) model
     SSE_null_b=((temp_df['Fc']-temp_df['Fc'].mean())**2).sum() # b model SSE

3de352270f2c06c12fff545a3db6d30f9a091251
Ian McHugh <ian_mchugh@fastmail.com>
2020-03-05 17:32:29 +1100

    Updated to py3 and refactored, also added histogram back in

--- random_error.py
+++ random_error.py
@@ -6,6 +6,7 @@ Created on Thu Jan 25 12:29:02 2018
 @author: ian
 """
 
+import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -60,109 +61,7 @@ class random_error(object):
         self.t_threshold = t_threshold
         self.ws_threshold = ws_threshold
         self.k_threshold = k_threshold
-        self.binned_error = self.get_flux_binned_sigma_delta()
 #------------------------------------------------------------------------------
-
-#------------------------------------------------------------------------------
-# Class methods
-#------------------------------------------------------------------------------
-
-    #--------------------------------------------------------------------------
-    def get_flux_binned_sigma_delta(self):
-
-        """
-        Calculate the daily differences and bin average as a function of
-        flux magnitude
-
-        Returns:
-            * Dictionary containing keys of 'night' and 'day', each of which
-              contains a pandas dataframe with estimates of binned mean flux
-              and corresponding sigma_delta estimate.
-        """
-
-    #----------------------------------------------------------------------
-    # Bin day and night data
-    def bin_time_series(mode):
-
-        try: assert mode in ['day', 'night']
-        except AssertionError: raise KeyError('"Mode" kwarg must be either '
-                                              '"day" or "night"')
-
-        def get_sigmas(df):
-            def calc(s):
-                return abs(s - s.mean()).mean() * np.sqrt(2)
-            return pd.DataFrame({'sigma_delta':
-                                  [calc(df.loc[df['quantile_label'] == x,
-                                                  'flux_diff']) for x in df['quantile_label'].unique()
-                                      .categories],
-                                 'mean':
-                                  [df.loc[df['quantile_label'] == x,
-                                             'flux_mean'].mean() for x in df['quantile_label'].unique()
-                                      .categories]})
-
-        if mode == 'day':
-
-
-
-        noct_df = filter_df.loc[filter_df.Fsd_mean < self.noct_threshold,
-                                ['flux_mean', 'flux_diff']]
-        day_df = filter_df.loc[filter_df.Fsd_mean > self.noct_threshold,
-                               ['flux_mean', 'flux_diff']]
-
-        nocturnal_propn = float(len(noct_df)) / len(filter_df)
-        num_cats_night = int(round(self.num_bins * nocturnal_propn))
-        num_cats_day = self.num_bins - num_cats_night
-
-        noct_df['quantile_label'] = pd.qcut(noct_df.flux_mean, num_cats_night,
-                                            labels = np.arange(num_cats_night))
-        noct_group_df = get_sigmas(noct_df)
-
-        day_df['quantile_label'] = pd.qcut(day_df.flux_mean, num_cats_day,
-                                           labels = np.arange(num_cats_day))
-        day_group_df = get_sigmas(day_df)
-
-        return day_group_df, noct_group_df
-    #----------------------------------------------------------------------
-
-    #--------------------------------------------------------------------------
-    def get_differenced_time_series(self, keep_met_diffs=False):
-
-        """
-        Returns the day-differenced time series with cases that fell outside
-        meteorological constraints - or nans - dropped
-
-        Args:
-            * keep_met_diffs (bool): returns the differences in the
-              meteorological constraints if true, otherwise they're dropped
-        """
-
-        diff_df = pd.DataFrame(index = self.df.index)
-        for var in ['flux', 'Ta', 'Fsd', 'Ws']:
-            var_name = var + '_diff'
-            temp = self.df[var] - self.df[var].shift(self.recs_per_day)
-            diff_df[var_name] = temp if var == 'flux' else abs(temp)
-        diff_df['flux_mean'] = (self.df['flux_mean'] + self.df['flux_mean']
-                                .shift(self.recs_per_day)) / 2
-        diff_df['Fsd_mean'] = (self.df['Fsd'] +
-                               self.df['Fsd'].shift(self.recs_per_day)) / 2
-        keep_bool = ((diff_df['Ws_diff'] < self.ws_threshold) &
-                     (diff_df['Ta_diff'] < self.t_threshold) &
-                     (diff_df['Fsd_diff'] < self.k_threshold))
-        diff_df = diff_df.loc[keep_bool].dropna()
-        if keep_met_diffs: return diff_df
-        return diff_df[['flux_diff', 'flux_mean', 'Fsd_mean']].dropna()
-    #--------------------------------------------------------------------------
-
-
-        #----------------------------------------------------------------------
-        # Main routine
-        #----------------------------------------------------------------------
-
-        diff_df = difference_time_series()
-        day_df, noct_df = bin_time_series()
-        return {'day': day_df, 'night': noct_df}
-
-    #--------------------------------------------------------------------------
 
     #--------------------------------------------------------------------------
     def estimate_random_error(self):
@@ -200,6 +99,89 @@ class random_error(object):
     #--------------------------------------------------------------------------
 
     #--------------------------------------------------------------------------
+    def get_differenced_time_series(self, keep_met_diffs=False):
+
+        """
+        Returns the day-differenced time series with cases that fell outside
+        meteorological constraints - or nans - dropped
+
+        Args:
+            * keep_met_diffs (bool): returns the differences in the
+              meteorological constraints if true, otherwise they're dropped
+        """
+
+        diff_df = pd.DataFrame(index = self.df.index)
+        for var in ['flux', 'Ta', 'Fsd', 'Ws']:
+            var_name = var + '_diff'
+            temp = self.df[var] - self.df[var].shift(self.recs_per_day)
+            diff_df[var_name] = temp if var == 'flux' else abs(temp)
+        diff_df['flux_mean'] = (self.df['flux_mean'] + self.df['flux_mean']
+                                .shift(self.recs_per_day)) / 2
+        diff_df['Fsd_mean'] = (self.df['Fsd'] +
+                               self.df['Fsd'].shift(self.recs_per_day)) / 2
+        keep_bool = ((diff_df['Ws_diff'] < self.ws_threshold) &
+                     (diff_df['Ta_diff'] < self.t_threshold) &
+                     (diff_df['Fsd_diff'] < self.k_threshold))
+        diff_df = diff_df.loc[keep_bool].dropna()
+        if keep_met_diffs: return diff_df
+        return diff_df[['flux_diff', 'flux_mean', 'Fsd_mean']].dropna()
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def get_flux_binned_sigma_delta(self):
+
+        """
+        Calculate the daily differences and bin average as a function of
+        flux magnitude
+
+        Returns:
+            * Dictionary containing keys of 'night' and 'day', each of which
+              contains a pandas dataframe with estimates of binned mean flux
+              and corresponding sigma_delta estimate.
+        """
+
+        # try: assert mode in ['day', 'night']
+        # except AssertionError: raise KeyError('"Mode" kwarg must be either '
+        #                                       '"day" or "night"')
+
+        def calc_laplace_sd(s): return abs(s - s.mean()).mean() * np.sqrt(2)
+
+        def get_sigmas(this_df):
+
+            func = calc_laplace_sd
+            sig_del = (
+                [func(this_df.loc[this_df['quantile_label'] == x, 'flux_diff'])
+                 for x in this_df['quantile_label'].unique().categories])
+            mean = [this_df.loc[this_df['quantile_label'] == x, 'flux_mean'].mean()
+                    for x in this_df['quantile_label'].unique().categories]
+            return pd.DataFrame({'sigma_delta': sig_del, 'mean': mean})
+
+
+
+        # if mode == 'day':
+        df = self.get_differenced_time_series()
+
+        noct_df = df.loc[df.Fsd_mean < self.noct_threshold,
+                         ['flux_mean', 'flux_diff']]
+        day_df = df.loc[df.Fsd_mean > self.noct_threshold,
+                        ['flux_mean', 'flux_diff']]
+
+        nocturnal_propn = float(len(noct_df)) / len(df)
+        num_cats_night = int(round(self.num_bins * nocturnal_propn))
+        num_cats_day = self.num_bins - num_cats_night
+
+        noct_df['quantile_label'] = pd.qcut(noct_df.flux_mean, num_cats_night,
+                                            labels = np.arange(num_cats_night))
+        noct_group_df = get_sigmas(noct_df)
+
+        day_df['quantile_label'] = pd.qcut(day_df.flux_mean, num_cats_day,
+                                           labels = np.arange(num_cats_day))
+        day_group_df = get_sigmas(day_df)
+
+        return {'day': day_group_df, 'night': noct_group_df}
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
     # Calculate basic regression statistics
     def get_regression_statistics(self):
 
@@ -220,9 +202,50 @@ class random_error(object):
     #--------------------------------------------------------------------------
 
     #--------------------------------------------------------------------------
-    def plot_data(self, flux_units = '\mu mol\/CO_2\/m^{-2}\/s^{-1}'):
 
-        data_dict = self.binned_error
+    def plot_histogram(self):
+
+        def myround(x,base=10):
+            return int(base*round(x/base))
+
+        # Instantiate plot
+        fig, ax1 = plt.subplots(1, 1, figsize = (14, 8))
+        fig.patch.set_facecolor('white')
+
+        # Calculate scaling parameter for Laplace (sigma / sqrt(2)) and Gaussian
+        # (sigma) distributions over entire dataset
+        df = self.get_differenced_time_series()
+        beta = (abs(df.flux_diff - df.flux_diff.mean())).mean()
+        sig = df.flux_diff.std()
+
+        # Get edge quantiles and range, then calculate Laplace pdf over range
+        x_low = myround(np.percentile(df.flux_diff, 0.5))
+        x_high = myround(np.percentile(df.flux_diff, 99.5))
+        x_range = (x_high - x_low)
+        x = np.arange(x_low, x_high, 1 / (x_range * 10.))
+        pdf_laplace = np.exp(-abs(x / beta)) / (2. * beta)
+        pdf_gaussian = stats.norm(loc=0, scale=sig).pdf(x)
+
+        # Plot normalised histogram with Laplacian and Gaussian pdfs
+        ax1.hist(np.array(df.flux_diff), bins = 200,
+                 range = [x_low, x_high], density=True, color = '0.7',
+                 edgecolor = 'none')
+        ax1.plot(x,pdf_laplace,color='black', label='Laplacian')
+        ax1.plot(x,pdf_gaussian,color='black', linestyle = '--',
+                label='Gaussian')
+        ax1.set_xlabel(r'$\delta\/(\mu mol\/m^{-2} s^{-1}$)',fontsize=18)
+        ax1.set_ylabel('$Frequency$', fontsize=18)
+        ax1.axvline(x=0, color = 'black', lw=0.5)
+        ax1.tick_params(axis = 'x', labelsize = 14)
+        ax1.set_yticks([0, 0.1, 0.2, 0.3, 0.4])
+        ax1.tick_params(axis = 'y', labelsize = 14)
+        ax1.legend(frameon=False)
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def plot_sigma_delta(self, flux_units = '\mu mol\/CO_2\/m^{-2}\/s^{-1}'):
+
+        data_dict = self.get_flux_binned_sigma_delta()
         stats_dict = self.get_regression_statistics()
 
         colour_dict = {'day': 'C1', 'night': 'C0'}
